5533cd0caf9e137d228d962c5ba34f6d4b554da0
Shailendra Kushwaha <learning4succedding@gmail.com>
2020-02-14 22:41:51 +0200

    app component setup

---
+++ src/pages/HomePage.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const HomePage = () => (
+  <>
+    <h1>Hello, welcome to my blog!</h1>
+    <p>
+      Welcome to my blog! Lorem ipsum dolor sit amet, consectetur adipiscing
+      elit. Vivamus commodo, mauris non porta sollicitudin, tortor tellus
+      posuere ex, sit amet accumsan sapien tortor id lectus. Sed sagittis justo
+      id augue consectetur pharetra. Maecenas sed magna eu magna molestie
+      tristique id ut sem. Sed vulputate fermentum tortor, eu ullamcorper diam
+      varius non.{" "}
+    </p>
+    <p>
+      Nullam nec sagittis sem. Praesent auctor orci at nisi commodo sodales.
+      Fusce blandit augue et nisl eleifend hendrerit. Fusce semper hendrerit
+      turpis eu dignissim. Vestibulum neque elit, semper nec lacus ac, vulputate
+      consequat tortor. Praesent tempor nunc sit amet lacus vestibulum, sit amet
+      ornare arcu iaculis.{" "}
+    </p>
+    <p>
+      Pellentesque eleifend diam lacus, vel feugiat quam commodo non. Vestibulum
+      ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia
+      Curae; Nunc sagittis quam dui, a pretium dui molestie nec.
+    </p>
+  </>
+);
+
+export default HomePage;
