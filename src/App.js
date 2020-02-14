ff7db4482bf35b4cfca6db678ffd5fbe0d1cf1f1
Shailendra Kushwaha <learning4succedding@gmail.com>
2020-02-15 01:08:00 +0200

    navbar link added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import NavBar from "./NavBar";
 
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
@@ -11,11 +12,12 @@ function App() {
   return (
     <Router>
       <div className="App">
+        <NavBar />
         <div id="page-body">
           <Route path="/" component={HomePage} exact />
-          <Route path="/about" component={AboutPage} exact />
-          <Route path="/articles-list" component={ArticlesList} exact />
-          <Route path="/article" component={ArticlePage} exact />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/articles-list" component={ArticlesList} />
+          <Route path="/article" component={ArticlePage} />
         </div>
       </div>
     </Router>
