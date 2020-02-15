fe3187286c3dd2a778b9b4a650c627495a1d6515
Shailendra Kushwaha <learning4succedding@gmail.com>
2020-02-15 02:05:19 +0200

    added changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import ArticlePage from "./pages/ArticlePage";
-import ArticlesList from "./pages/ArticlesList";
+import ArticlesListPage from "./pages/ArticlesListPage";
 
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
         <div id="page-body">
           <Route path="/" component={HomePage} exact />
           <Route path="/about" component={AboutPage} />
-          <Route path="/articles-list" component={ArticlesList} />
+          <Route path="/articles-list" component={ArticlesListPage} />
           <Route path="/article/:name" component={ArticlePage} />
         </div>
       </div>
